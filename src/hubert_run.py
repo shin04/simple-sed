db5f350ea816f353f432a4b6bafcaeac38470c8e
shin04 <daikon0413@gmail.com>
2021-08-17 11:07:29 +0900

    implement validation process

--- src/hubert_run.py
+++ src/hubert_run.py
@@ -14,7 +14,7 @@ import torch.optim as optim
 
 from model.crnn import CRNN
 from dataset.hubert_feat import HuBERTDataset
-from training.hubert_train import train
+from training.hubert_train import train, valid
 from utils.callback import EarlyStopping
 from utils.param_util import log_params_from_omegaconf_dict
 
@@ -34,16 +34,18 @@ def run(cfg: DictConfig) -> None:
 
     feat_path = Path(cfg['dataset']['feat_path'])
     train_meta = Path(cfg['dataset']['train_meta'])
-    # valid_meta = Path(cfg['dataset']['valid_meta'])
+    valid_meta = Path(cfg['dataset']['valid_meta'])
     # test_meta = Path(cfg['dataset']['test_meta'])
     train_weak_label = Path(cfg['dataset']['train_weak_label'])
-    # valid_weak_label = Path(cfg['dataset']['valid_weak_label'])
+    valid_weak_label = Path(cfg['dataset']['valid_weak_label'])
     # test_weak_label = Path(cfg['dataset']['test_weak_label'])
     # train_duration = Path(cfg['dataset']['train_duration'])
     # valid_duration = Path(cfg['dataset']['valid_duration'])
     # test_duration = Path(cfg['dataset']['test_duration'])
     model_path = Path(cfg['model']['save_path']) / f'{ex_name}-{ts}-best.pt'
 
+    sr = cfg['dataset']['sr']
+    sec = cfg['dataset']['sec']
     net_pooling_rate = cfg['dataset']['net_pooling_rate']
 
     n_epoch = cfg['training']['n_epoch']
@@ -61,9 +63,20 @@ def run(cfg: DictConfig) -> None:
 
     """prepare datasets"""
     train_dataset = HuBERTDataset(
-        feat_path=feat_path,
+        feat_path=feat_path / 'train',
         metadata_path=train_meta,
         weak_label_path=train_weak_label,
+        sr=sr,
+        sec=sec,
+        net_pooling_rate=net_pooling_rate,
+        transforms=None
+    )
+    valid_dataset = HuBERTDataset(
+        feat_path=feat_path / 'valid',
+        metadata_path=valid_meta,
+        weak_label_path=valid_weak_label,
+        sr=sr,
+        sec=sec,
         net_pooling_rate=net_pooling_rate,
         transforms=None
     )
@@ -71,6 +84,13 @@ def run(cfg: DictConfig) -> None:
         train_dataset, batch_size=batch_size, shuffle=True,
         num_workers=num_workers, pin_memory=pin_memory
     )
+    valid_dataloader = DataLoader(
+        valid_dataset, batch_size=batch_size, shuffle=False,
+        num_workers=num_workers, pin_memory=pin_memory
+    )
+
+    if train_dataset.classes != valid_dataset.classes:
+        raise RuntimeError("class_map is wrong")
 
     """prepare training"""
     model = CRNN(
@@ -98,10 +118,37 @@ def run(cfg: DictConfig) -> None:
                 global_step, model, train_dataloader, device, optimizer, criterion
             )
 
+            (
+                valid_strong_loss,
+                valid_weak_loss,
+                valid_tot_loss,
+                valid_weak_f1,
+                valid_sed_evals
+            ) = valid(
+                model, valid_dataloader, device, criterion,
+                valid_dataset.class_map,
+                cfg['training']['thresholds'],
+                cfg['training']['sed_eval_thr'],
+                valid_meta
+            )
+
             mlflow.log_metric('train/strong/loss',
                               train_strong_loss, step=epoch)
             mlflow.log_metric('train/weak/loss', train_weak_loss, step=epoch)
             mlflow.log_metric('train/tot/loss', train_tot_loss, step=epoch)
+            mlflow.log_metric('valid/strong/loss',
+                              valid_strong_loss, step=epoch)
+            mlflow.log_metric('valid/weak/loss', valid_weak_loss, step=epoch)
+            mlflow.log_metric('valid/tot/loss', valid_tot_loss, step=epoch)
+            mlflow.log_metric('valid/weak/f1', valid_weak_f1, step=epoch)
+            mlflow.log_metric('valid/sed_eval/segment/class_wise_f1',
+                              valid_sed_evals['segment']['class_wise_f1'], step=epoch)
+            mlflow.log_metric('valid/sed_eval/segment/overall_f1',
+                              valid_sed_evals['segment']['overall_f1'], step=epoch)
+            mlflow.log_metric('valid/sed_eval/event/class_wise_f1',
+                              valid_sed_evals['event']['class_wise_f1'], step=epoch)
+            mlflow.log_metric('valid/sed_eval/event/overall_f1',
+                              valid_sed_evals['event']['overall_f1'], step=epoch)
 
             print(
                 '====================\n'
@@ -112,6 +159,19 @@ def run(cfg: DictConfig) -> None:
                 f'train loss(strong):{train_strong_loss: .4f}, '
                 f'train loss(weak):{train_weak_loss: .4f}, '
                 f'train loss(total):{train_tot_loss: .4f}'
+            )
+            print(
+                '[VALID]\n'
+                f'valid loss(strong):{valid_strong_loss: .4f}, '
+                f'valid loss(weak):{valid_weak_loss: .4f}, '
+                f'valid loss(total):{valid_tot_loss: .4f}'
+            )
+            print(
+                '[VALID SED EVAL]\n'
+                f'segment/class_wise_f1:{valid_sed_evals["segment"]["class_wise_f1"]: .4f}',
+                f'segment/overall_f1:{valid_sed_evals["segment"]["overall_f1"]: .4f}',
+                f'event/class_wise_f1:{valid_sed_evals["event"]["class_wise_f1"]: .4f}',
+                f'event/overall_f1:{valid_sed_evals["event"]["overall_f1"]: .4f}',
             )
 
             if best_loss > train_tot_loss:
