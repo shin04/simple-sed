739e6a7f21a21aca0d8d736e94531c03fa20a1f6
shin04 <daikon0413@gmail.com>
2021-11-03 15:38:57 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+$WORK_DIR=$PWD
+
 #----- synbolic link -----#
 ln -s $HOME/mrnas02home/datasets/urban_sed/meta ./meta
 ln -s $HOME/nas02home/models/sed ./models
@@ -6,3 +8,15 @@ mkdir ./dataset
 cd ./dataset
 ln -s $HOME/mrnas02home/datasets/urban_sed/audio ./audio
 ln -s $HOMEnas02home/dataset/hubert_feat/urbansed_audioset ./feat
+
+
+#----- venv -----#
+cd $WORK_DIR
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install install \
+    torch==1.9.0+cu111 \
+    torchvision==0.10.0+cu111 \
+    torchaudio==0.9.0 \
+    -f https://download.pytorch.org/whl/torch_stable.html
