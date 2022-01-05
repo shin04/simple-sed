8b59696fd0467b0d5e718ee42415e92e9cdeb13d
shin04 <daikon0413@gmail.com>
2022-01-05 19:37:32 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -15,6 +15,7 @@ touch .env.mlflow
 echo "TRACKING_URL=$WORK_DIR/results/mlflow" > .env.mlflow
 
 #----- venv -----#
+# downstream task
 cd $WORK_DIR
 python3 -m venv .venv
 source .venv/bin/activate
@@ -24,3 +25,19 @@ pip install install \
     torchvision==0.10.0+cu111 \
     torchaudio==0.9.0 \
     -f https://download.pytorch.org/whl/torch_stable.html
+deactivate
+
+# finetuning task
+python3 -m venv .venv-finetune
+source .venv-finetune/bin/activate
+pip install -r ./requirements.finetune.txt
+pip install install \
+    torch==1.9.0+cu111 \
+    torchvision==0.10.0+cu111 \
+    torchaudio==0.9.0 \
+    -f https://download.pytorch.org/whl/torch_stable.html
+cd ../fairseq
+pip install --editable ./
+deactivate
+
+cd ../simple-sed
